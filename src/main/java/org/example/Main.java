a2a31dbda39d99e2e41404848666da5bab9ea61f
zc2020-imperial <zc2020@ic.ac.uk>
2022-11-22 16:49:41 +0000

    v1.1_devel-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,12 @@ public class Main {
         for (int i = 1; i<=10;i++){
             System.out.println(i);
         }
+        int sum_val = 0;
+        for (int i = 30; i<=40;i++){
+
+            sum_val = sum.add(sum_val,i);
+
+        }
+        System.out.println(sum_val);
     }
 }
---
+++ src/main/java/org/example/sum.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class sum {
+    public static int add(int num1, int num2){
+        int a = num1;
+        int b = num2;
+        return a + b;
+    }
+}
