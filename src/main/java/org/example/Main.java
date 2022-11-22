ab7f67a66d0cdfab317fbc321247aeeb553c8c65
zc2020-imperial <zc2020@ic.ac.uk>
2022-11-22 16:55:35 +0000

    v1.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Good Morning, universe!");
-        System.out.println("Hello world!");
         for (int i = 1; i<=10;i++){
             System.out.println(i);
         }
